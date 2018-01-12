94f8897f30618c096eaa0c18364210c88f42759a
ishmael <ishmael@sheban>
2018-01-12 23:54:55 +0300

    Updated Grower Profile Test Suites

---
+++ Test Suites/Home/TST-GrowerProfile.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TST-GrowerProfile</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b424b37e-8139-4759-9a97-88c9835e0524</testSuiteGuid>
+   <testCaseLink>
+      <guid>eda9ff00-ea64-4852-bf9b-7e1afd145b50</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Home/Grower Profile/Profile Card Attributes/TS-ProfileCardAttributes/TC-ProfileCardAttributes</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ff732034-5d21-4a4d-b10f-232c927789e7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Home/Grower Profile/Profile Card Attributes/VT-VerifyProfileCardAttributes/VT-VerifyProfileCardAttributes</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c6a06876-286d-4be8-8557-291a9388fc0f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Home/Grower Profile/Profile Tabs/TS-ProfileTabs/TC-ProfileTabs</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a5e4d3c4-e221-4300-afc3-05291006f186</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Home/Grower Profile/Profile Tabs/VT-VerifyProfileTabs/VT-VerifyProfileTabs</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>edaa84d6-0e3d-4a4c-9725-f22c87bd1ec7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Home/Grower Profile/Search Grower/TS-Search Grower/TC-SearchGrowerName</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e291c2ba-3b76-4332-9a9d-b86a4a2ae75b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Home/Grower Profile/Search Grower/VT-Verify Search Grower/VT-CaptureSearchedGrowerName</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
